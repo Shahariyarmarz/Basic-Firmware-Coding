424d4ae760a56872e75032b9f7cff2f6a469aaf5
Shahariyarmarz <151366540+Shahariyarmarz@users.noreply.github.com>
2023-12-15 23:40:55 +0600

    Add files via upload

---
+++ DAQ Code/Reading_and_storing_binary_sensor_data_Final/DAQ_functions.ino
@@ -0,0 +1,30 @@
+void read_analog_sensors(void){
+  if (DATA_READ_ENABLE) {
+		ADC_BUFFER[0][ADC_BUF_END_INDX] = (analogRead(A0) + analogRead(A0) + analogRead(A0)) / 3;
+		// ADC_BUFFER[1][ADC_BUF_END_INDX] = (analogRead(A1) + analogRead(A1) + analogRead(A1)) / 3;
+		// ADC_BUFFER[2][ADC_BUF_END_INDX] = (analogRead(A2) + analogRead(A2) + analogRead(A2)) / 3;
+		ADC_BUF_END_INDX = (ADC_BUF_END_INDX + 1) % ADC_BUF_SIZE;
+		if (ADC_BUF_END_INDX == ADC_BUF_START_INDX) {
+			Serial.println("Buffer Overflow!!!!!");
+		}
+	}
+}
+
+void logData(uint16_t ADC_buf_copy[1][ADC_COPY_SIZE], uint16_t size) {
+  // uint16_t data[ANALOG_COUNT];
+  if (file) {
+        for (size_t i = 0; i < size; i++) {
+            file.write((const uint8_t *)&ADC_buf_copy[0][i], sizeof(ADC_buf_copy[0][i]));
+            // file.print(String(ADC_buf_copy[0][i]));
+            // dataFile.write((const uint8_t *)&ADC_buf_copy, sizeof(ADC_buf_copy));
+            // file.println(",");
+            // file.print(String(ADC_buf_copy[1][i]));
+            // file.print(",");
+            // file.println(String(ADC_buf_copy[2][i]));
+        }
+        file.close();
+    } else {
+        Serial.println("error opening datalog.dat");
+    }
+
+}
